7f74a93419e0b0200758348345b3ed753df1e9bb
Rehana Ali <160489687+Rehana-Ali@users.noreply.github.com>
2024-05-20 10:03:44 -0700

    Add files via upload

---
+++ Q23.unchanged magician array/app.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function show_magicians(magicians) {
+    for (const magician of magicians) {
+        //  console.log(magician)
+        console.log(`${magician}  the Greate`);
+    }
+}
+// 
+const magicianName = ["Usman", "Haseeb", "Wajahat"];
+show_magicians(magicianName);
