b9678167d6e9ad89bedb0bc4286d99b68189f4bc
Rehana Ali <160489687+Rehana-Ali@users.noreply.github.com>
2024-05-20 10:45:24 -0700

    Add files via upload

---
+++ Q36. T-shirts/app.js
@@ -0,0 +1,6 @@
+"use strict";
+function makeShirt(size, text) {
+    console.log(`You order a ${size} shirt that says ${text}`);
+}
+makeShirt('large', '"I love typescript"');
+makeShirt('medium', '"I need a big shirt"');
