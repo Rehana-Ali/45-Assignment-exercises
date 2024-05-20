5925b527ee7377af1a1f95fefdb5f17066e27ff8
Rehana Ali <160489687+Rehana-Ali@users.noreply.github.com>
2024-05-20 10:32:42 -0700

    Add files via upload

---
+++ Q39. City Names/app.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function citycountry(city, country) {
+    return `${city}, ${country}`;
+}
+let c1 = citycountry('lahore', 'Pkistan');
+let c2 = citycountry('tokyo', 'japan');
+let c3 = citycountry('peris', 'france');
+console.log(c1);
+console.log(c2);
+console.log(c3);
