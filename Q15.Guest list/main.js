9d8a8478d94bf81c9af64bd996db97494c017326
Rehana Ali <160489687+Rehana-Ali@users.noreply.github.com>
2024-05-23 10:41:33 -0700

    Delete Q15.Guest list directory
    deleted

--- Q15.Guest list/main.js
+++
@@ -1,4 +0,0 @@
-let guestarray = ["haya", "aresha", "rameema"];
-// guestarray.map((items)=> console.log(`Hello ${items}, you are invited to dinner.`));
-guestarray.forEach(oneGuest => console.log(`Hello ${oneGuest}, you are invited to dinner.`));
-export {};
