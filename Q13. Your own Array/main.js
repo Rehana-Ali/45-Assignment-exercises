57bbe8d4c4f590647b72d27c1bf669f383a813c5
Rehana Ali <160489687+Rehana-Ali@users.noreply.github.com>
2024-05-23 10:54:28 -0700

    Add files via upload

---
+++ Q13. Your own Array/main.js
@@ -0,0 +1,3 @@
+let transportation = ["Honda Motercycle", "Honda City", "Honda Civic", "BMW"];
+transportation.map((items) => console.log("I Would like to own a ", items));
+export {};
---
+++ Q13. Your own Array/main.ts
@@ -0,0 +1,3 @@
+let transportation:string[] = ["Honda Motercycle","Honda City","Honda Civic","BMW"];
+
+transportation.map((items)=> console.log("I Would like to own a ",items)); 
