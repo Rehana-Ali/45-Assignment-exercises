8a8698bcf32a747b8d47ffc3ab7017f946b08e04
Rehana Ali <160489687+Rehana-Ali@users.noreply.github.com>
2024-05-23 07:00:10 -0700

    Add files via upload

---
+++ Q29. Favourite Fruit/app.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const favouriteFruites = ["Banana", "apple", "Strawberry"];
+if (favouriteFruites.includes("Banana")) {
+    console.log("you really like Bananas!");
+}
+if (favouriteFruites.includes("Apple")) {
+    console.log("you really like Apple!");
+}
+if (favouriteFruites.includes("orange")) {
+    console.log("you really like orange!");
+}
+else {
+    console.log("orange are not in your favorite fruits list.");
+}
+if (favouriteFruites.includes("Strawberry")) {
+    console.log("you really like Strawberries!");
+}
+if (favouriteFruites.includes("Grapes")) {
+    console.log("you really like Grapes!");
+}
+else {
+    console.log("Grapes are not in your favorite  list.");
+}
---
+++ Q29. Favourite Fruit/app.ts
@@ -0,0 +1,29 @@
+const favouriteFruites: string [] = ["Banana", "apple", "Strawberry"];
+
+if(favouriteFruites.includes("Banana")){
+    console.log("you really like Bananas!")
+}
+
+if(favouriteFruites.includes("Apple")){
+    console.log("you really like Apple!")
+}
+
+
+if(favouriteFruites.includes("orange")){
+    console.log("you really like orange!")
+}
+else{
+console.log("orange are not in your favorite fruits list.");
+}
+
+if(favouriteFruites.includes("Strawberry")){
+    console.log("you really like Strawberries!")
+}
+
+if(favouriteFruites.includes("Grapes")){
+    console.log("you really like Grapes!")
+}
+
+else{
+    console.log("Grapes are not in your favorite  list.");
+}
