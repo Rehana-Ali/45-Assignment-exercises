afd0d9d3c4d5f7c36691306ff3ab191f58893122
Rehana Ali <160489687+Rehana-Ali@users.noreply.github.com>
2024-05-23 06:56:35 -0700

    Add files via upload

---
+++ Q30. Hello Admin/app.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let users = ["Armish", "Arsalan", "Imtiaz", "Bilawal"];
+users = [];
+if (users.length === 0) {
+    console.log("we need to find some users!");
+}
---
+++ Q30. Hello Admin/app.ts
@@ -0,0 +1,6 @@
+let users:string [] = ["Armish","Arsalan","Imtiaz","Bilawal"];
+
+users = []
+if(users.length === 0){
+    console.log("we need to find some users!")
+}
