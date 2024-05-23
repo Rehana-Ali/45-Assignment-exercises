fb38b47b544550d7c7ed570f096ad9f7f43a386a
Rehana Ali <160489687+Rehana-Ali@users.noreply.github.com>
2024-05-23 08:21:19 -0700

    Add files via upload

---
+++ Q19. Dinner Guest/app.js
@@ -0,0 +1,6 @@
+"use strict";
+let guestArray = ["haya", "aresha", "rameema"];
+console.log(guestArray.length, "people are invited to dinner");
+let guest = ["Ali", "Ahmed", "Rashid"];
+let guestList = guest.length;
+console.log(`We are inviting totat ${guestList} guest.`);
