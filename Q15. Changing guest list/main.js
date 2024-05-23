1742eaee77870d76deb06663d8f24a3898912ca0
Rehana Ali <160489687+Rehana-Ali@users.noreply.github.com>
2024-05-23 10:44:52 -0700

    Add files via upload

---
+++ Q15. Changing guest list/main.js
@@ -0,0 +1,11 @@
+// name array
+let guestArray = ["haya", "aresha", "rameema"];
+// can not make dinner
+let canNotAttend = "rameema";
+console.log(`${canNotAttend} can not make it, for dinner.`);
+// invite new guest
+let newGuest = "suhana";
+guestArray[guestArray.indexOf(canNotAttend)] = newGuest;
+// send message
+guestArray.map((item) => console.log(`Hello,${item} you are invited to dinner.`));
+export {};
---
+++ Q15. Changing guest list/main.ts
@@ -0,0 +1,48 @@
+// name array
+let guestArray:string[] = [ "haya", "aresha", "rameema" ];
+
+
+// can not make dinner
+let canNotAttend:string = "rameema";
+console.log(`${canNotAttend} can not make it, for dinner.`);
+
+
+// invite new guest
+let newGuest :string = "suhana";
+guestArray[guestArray.indexOf(canNotAttend)]=newGuest;
+
+
+
+
+// send message
+guestArray.map((item)=> console.log(`Hello,${item} you are invited to dinner.`));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
