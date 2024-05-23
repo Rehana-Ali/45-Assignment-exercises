34d531168698fd00be3f63810c3e4e9a0be4729f
Rehana Ali <160489687+Rehana-Ali@users.noreply.github.com>
2024-05-23 07:21:45 -0700

    Add files via upload

---
+++ Q26.If else with colour2/main.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let alienColor = "green";
+alienColor = "red";
+if (alienColor === "green") {
+    console.log("player just earned  5 points!");
+}
+else {
+    console.log("player just earned 10  points!");
+}
---
+++ Q26.If else with colour2/main.ts
@@ -0,0 +1,10 @@
+let alienColor:string = "green";
+alienColor ="red"
+
+if (alienColor ==="green"){
+    console.log("player just earned  5 points!");
+}
+
+else{
+console.log("player just earned 10  points!");
+}
