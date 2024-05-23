a6dea2033e92a2d8d10f8edf67b6eea65f29ce11
Rehana Ali <160489687+Rehana-Ali@users.noreply.github.com>
2024-05-23 07:06:05 -0700

    Add files via upload

---
+++ Q27. If else with colour#3/main.ts
@@ -0,0 +1,16 @@
+let alienColor:string = "green";
+
+// Virsion 1 of the program
+if (alienColor ==="green"){
+    console.log(" virsion 1: player just earned  5 points!");
+}
+else if(alienColor === "yellow"){
+    console.log("player just earned  10 points!");
+}
+else if(alienColor === "red"){
+    console.log("player just earned  15  points!");
+}
+else{
+    console.log("please select right color");
+}
+
