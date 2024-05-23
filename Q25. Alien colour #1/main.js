926b9fef00055d858b75727bb3d7f205fab7d78a
Rehana Ali <160489687+Rehana-Ali@users.noreply.github.com>
2024-05-23 07:28:05 -0700

    Add files via upload

---
+++ Q25. Alien colour #1/main.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Create a veriable called alien_color
+let alienColor = "green";
+if (alienColor === "green") {
+    console.log("player just earned  5 points!");
+}
+alienColor = "red";
+if (alienColor === "green") {
+    console.log("player just earned 5 points!");
+}
+else {
+    console.log("My it cours is superb");
+}
---
+++ Q25. Alien colour #1/main.ts
@@ -0,0 +1,17 @@
+// Create a veriable called alien_color
+let alienColor:string = "green";
+
+
+if (alienColor ==="green"){
+    console.log("player just earned  5 points!");
+}
+
+
+alienColor = "red";
+if (alienColor === "green"){
+    console.log("player just earned 5 points!");
+}
+
+else{
+console.log("My it cours is superb" )
+}
