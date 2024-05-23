937e3cb2257b3545fc9b975f1a2ffe7f27133dc0
Rehana Ali <160489687+Rehana-Ali@users.noreply.github.com>
2024-05-23 10:38:04 -0700

    Add files via upload

---
+++ Q15.Guest list/main.ts
@@ -0,0 +1,8 @@
+let guestarray:string[] = [ "haya", "aresha", "rameema" ];
+
+// guestarray.map((items)=> console.log(`Hello ${items}, you are invited to dinner.`));
+
+guestarray.forEach(oneGuest => console.log(`Hello ${oneGuest}, you are invited to dinner.`))
+
+
+
