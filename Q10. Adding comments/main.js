55c35b8ec96ec9ea5dd9cf896516350a57b39b7f
Rehana Ali <160489687+Rehana-Ali@users.noreply.github.com>
2024-05-23 11:07:28 -0700

    Add files via upload

---
+++ Q10. Adding comments/main.js
@@ -0,0 +1,9 @@
+// Auther: Rizwana Ali
+// date: 24 march,2024
+//I did Addition between two numbers.
+console.log(2 + 4);
+// / Auther: Rizwana Ali
+// date: 24 march,2024
+//I did subtraction between two numbers.
+console.log(10 - 4);
+export {};
