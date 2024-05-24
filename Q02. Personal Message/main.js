145da94c805ac29d4ab72d534148e1221f05673a
Rehana Ali <160489687+Rehana-Ali@users.noreply.github.com>
2024-05-24 01:07:45 -0700

    Add files via upload

---
+++ Q02. Personal Message/main.js
@@ -0,0 +1,4 @@
+let Name = "khushi";
+console.log(` “Hello ${Name}, would you like to learn some typescript today?”`);
+console.log("Hello " + Name + "," + " would you like to learn some python today?");
+export {};
---
+++ Q02. Personal Message/main.ts
@@ -0,0 +1,5 @@
+let Name:string = "Ahmed";
+
+console.log(` “Hello ${Name}, would you like to learn some typescript today?”`);
+
+console.log("Hello " + Name + "," +" would you like to learn some python today?")
