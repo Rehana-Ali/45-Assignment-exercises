0cacc72d927d4d3738c9b85b79361741acd83143
Rehana Ali <160489687+Rehana-Ali@users.noreply.github.com>
2024-05-24 01:01:18 -0700

    Add files via upload

---
+++ Q04. Famous Qoute#1/main.js
@@ -0,0 +1,4 @@
+console.log('Nelson Mandela once said, "Education is the most powerful weapon which you can use to change the world." ');
+let auther = "Nelson Mandela";
+console.log(`Once ${auther} said,"Education is the most powerful weapon which you can use to change the world."`);
+export {};
---
+++ Q04. Famous Qoute#1/main.ts
@@ -0,0 +1,14 @@
+console.log('Nelson Mandela once said, "Education is the most powerful weapon which you can use to change the world." ');
+
+
+
+
+
+
+
+
+
+let auther ="Nelson Mandela";
+
+console.log(`Once ${auther} said,"Education is the most powerful weapon which you can use to change the world."`)
+
