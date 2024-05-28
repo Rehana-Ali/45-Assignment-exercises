5ed51b5138ab425c09f91e3cfcc762f67cfacb8e
Rehana Ali <160489687+Rehana-Ali@users.noreply.github.com>
2024-05-27 21:55:43 -0700

    Update main.ts
    done some changes

--- Q10. Adding comments/main.ts
+++ Q10. Adding comments/main.ts
@@ -1,11 +1,11 @@
-// Auther: Rizwana Ali
+// Auther: Rehana Ali
 // date: 24 march,2024
 
 //I did Addition between two numbers.
 
 console.log(2+4);
 
-// / Auther: Rizwana Ali
+// / Auther: Rehana Ali
 // date: 24 march,2024
 
 //I did subtraction between two numbers.
